e4621ba0e7c8a9effef265ca08b192218c7f7dd4
Ryder-07 <kunals1209@gmail.com>
2025-12-02 00:20:10 +0530

    Added files for callback hell

---
+++ Basics/CallBack_Hell.js
@@ -0,0 +1,20 @@
+function setTimeoutPromisified(duration){
+    return new Promise(function(resolve){
+        setTimeout(resolve, duration);
+    })
+};
+
+
+//Promise Calling
+
+setTimeoutPromisified(1000).then(function(){
+    console.log("hi");
+    return setTimeoutPromisified(3000)
+}).then(function{
+    console.log("Hello");
+    return setTimeoutPromisified(5000)
+}).then(function () {
+    console.log("Hi There");
+});
+
+console.log("outside the callback hell");
