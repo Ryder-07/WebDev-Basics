b09f320aed270ce921df0d7a3a85ead7af2e0dc3
Ryder-07 <kunals1209@gmail.com>
2025-12-03 23:13:18 +0530

    file promise case

--- Basics/Async_await.js
+++ Basics/Async_await.js
@@ -4,14 +4,31 @@ function setTimeoutPromisified(duration){
     });
 }
 
-async function solve(){
-    await setTimeoutPromisified(1000);
-    console.log("hi");
-    await setTimeoutPromisified(3000);
-    console.log("Hey");
-    await setTimeoutPromisified(5000);
-    console.log("Hi there");
-}
+// async function solve(){
+//     await setTimeoutPromisified(1000);
+//     console.log("hi");
+//     await setTimeoutPromisified(3000);
+//     console.log("Hey");
+//     await setTimeoutPromisified(5000);
+//     console.log("Hi there");
+// }
 
 solve();
 console.log("this is outside the Async and Await")
+
+
+const fs = require("fs");
+
+
+function readFileAsync(){
+    return new Promise(function (resolve) {
+        fs.readFile("a.text","utf8",function(err,data){
+            resolve(data);
+        })
+    })
+}
+
+
+async function solve(){
+    await readFileAsync(1000);
+}
