caa7c302591345643d89425f0bce624a9021d102
Ryder-07 <kunals1209@gmail.com>
2025-12-04 22:48:01 +0530

    Added new routes to todo app

--- week-4-todo-app/index.js
+++ week-4-todo-app/index.js
@@ -6,4 +6,12 @@ app.get('/',function(req,res){
     res.send('hello world')
 })
 
-app.listen(3000)
+app.post('/',function(req,res){
+    res.send('hello from the post request handeler')
+})
+
+app.get('/asd',function(req,res){
+    res.send('hello world from the asd route')
+})
+
+app.listen(3000) //which port
