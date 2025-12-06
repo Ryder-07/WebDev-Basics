ea94b1835f21f9bd361c1366aa03ed69f359845c
Ryder-07 <kunals1209@gmail.com>
2025-12-06 22:19:21 +0530

    added multiply endpoint in inedx.js

--- WEEK_5/index.js
+++ WEEK_5/index.js
@@ -11,7 +11,14 @@ app.get("/sum", function(req,res){
     })
 })
 
+app.get("/multiply", function(req,res){
+    const a = req.query.a;
+    const b= req.query.b;
 
+    res.json({
+        answer: a*b
+    })
+})
 
 
 app.listen(3000);
