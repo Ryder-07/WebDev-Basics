b74de10f57081ba355c4608f52925b056a89e09b
Ryder-07 <kunals1209@gmail.com>
2025-12-09 00:38:32 +0530

    added skeleton for todo with db app

---
+++ Week_7/mongo_todo/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const mongo= require("mongoose");
+
+
+
+ 
+const app = express();
+
+app.post("/signup", function(req,res){
+
+})
+
+app.post("/signin", function(req,res){
+
+})
+
+app.post("/todo", function(req,res){
+
+})
+
+app.get("/todos", function(req,res){
+
+})
+
+app.listen(3000);
