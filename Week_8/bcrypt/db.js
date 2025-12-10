42edc69773cee2188c977b0e61fbc193e2bdb8f5
Ryder-07 <kunals1209@gmail.com>
2025-12-11 01:36:41 +0530

    added files for zod and bcrypt

---
+++ Week_8/bcrypt/db.js
@@ -0,0 +1,29 @@
+const mongo= require("mongoose");
+
+const schema= mongo.Schema;
+const ObjectId = mongo.ObjectId;
+
+
+const Users= new schema({
+    email: String,
+    password: String,
+    name: String
+})
+
+const Todo = new schema({
+    title: String,
+    done: Boolean,
+    userId: {
+  type: mongo.Schema.Types.ObjectId,
+  ref: "Users"
+}
+})
+
+const UserModel = mongo.model('Users',Users);
+const TodoModel= mongo.model('todos',Todo);
+
+
+module.exports={
+    UserModel: UserModel,
+    TodoModel: TodoModel
+}
---
+++ Week_8/bcrypt/index.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const { UserModel, TodoModel } = require('./db');
+const mongoose = require("mongoose");
+const bcrypt= require("bcrypt");
+
+
+
+const JWT_SECRET = "secret";
+mongoose.connect("")
+  .then(() => console.log("🔥 Mongo Connected Successfully"))
+  .catch((e) => console.log("❌ Mongo Connection Error:", e));
+
+
+
+const app = express();
+app.use(express.json());
+
+
+
+app.post("/signup",async function(req,res){
+    const email= req.body.email;
+    const password= req.body.password;
+    const name= req.body.name;
+
+    const hashedPassword = await await bcrypt.hash(password,5);
+    console.log(hashedPassword)
+
+    await UserModel.create({
+            name: name,
+            email: email,
+            password: hashedPassword 
+
+        })
+
+        res.json({
+            messsage: "You are logged in"
+        })
+})
+
+app.post("/signin",async function(req,res){
+    const email= req.body.email;
+    const password= req.body.password;
+    
+    const user =await UserModel.findOne({
+        email:email,
+        password: password
+    })
+    console.log(user);
+    if (user){
+        const  token =jwt.sign({
+            id:user._id.toString()
+        },JWT_SECRET);
+        res.json({
+                token:token
+        });
+    } else{
+        res.status(403).json({
+            message:"Incorrect credentials"
+        })
+        }
+})
+
+app.post("/todo",auth, async function(req,res){
+    const userId =req.userId;
+    const title = req.body.title;
+    const done =req.body.done;
+    console.log("User ID:", req.userId);
+    await TodoModel.create({
+        title,
+        userId,
+        done
+    })
+    res.json({
+        message: "Todo created"
+    })
+})
+
+app.get("/todos",auth,async function(req,res){
+    const userId=req.userId;
+    const todos = await TodoModel.find({
+        userId:userId
+    })
+    res.json({
+        todos
+    })
+})
+
+function auth(req,res,next){
+    const token = req.headers.token;
+    const decodedData = jwt.verify(token,JWT_SECRET);
+
+    if(decodedData){
+        
+            req.userId= decodedData.id;
+            next();
+    }
+    else{
+        res.json({
+            message: " Incorrect Username or password"
+        })
+    }
+    
+}
+
+app.listen(3000);
