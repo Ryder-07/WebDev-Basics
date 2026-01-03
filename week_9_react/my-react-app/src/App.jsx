62f27787f09afbf39e95c78e0a94e2d3260230c9
Ryder-07 <kunals1209@gmail.com>
2026-01-03 21:42:24 +0530

    added todos app functionality

--- week_9_react/my-react-app/src/App.jsx
+++ week_9_react/my-react-app/src/App.jsx
@@ -4,18 +4,45 @@ import './App.css'
 
 
 export default function App() {
-  const [ count, setCount] = useState(0); // hooks
+
+  const [ todos, setTodos] = useState([{
+    title: "go to gym",
+    desc: "hit the gym regularly",
+    done: false  
+  }]);
 
 
 
-  function onClickHandler(){
-    setCount(count +1)
+  function addTodo(){
+    let newArray = [];
+    for( let i = 0 ; i< todos.length; i++){
+      newArray.push(todos[i]);
+    }
+    newArray.push ({
+      title: document.getElementById("title").value,
+      description: document.getElementById("desc").value,
+      done: true
+    });
+    setTodos(newArray);
   }
-
-
   return (
     <div>
-      <button id = "btn" onClick={onClickHandler}>Counter {count}</button>
+      <input id='title ' type="text" placeholder='Title' />
+      <br />
+      <input id='description' type="text" placeholder='description' />
+      <br />
+      <button onClick={addTodo}>add to todo</button>
+      <br />
+      {JSON.stringify(todos)}
     </div>
   )
+}
+
+
+function Todo(props){
+  return (<div>
+    <h1>{props.title}</h1>
+    <h2>{props.description}</h2>
+    <h1>{props.done}</h1>
+  </div>)
 }
