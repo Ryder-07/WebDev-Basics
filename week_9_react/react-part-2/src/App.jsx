5da2a09dbbe5c4d959987ddc46b1a1624cd60c0c
Ryder-07 <kunals1209@gmail.com>
2026-01-04 13:44:40 +0530

    added a clock based functionalty for secound counter

--- week_9_react/react-part-2/src/App.jsx
+++ week_9_react/react-part-2/src/App.jsx
@@ -1,14 +1,56 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <div>
+//       <Counter></Counter>
+//     </div>
+   
+//   )
+// }
+
+// function Counter(){
+//   const [count, setCount] = useState(0);
+
+//   function increaseCount(){
+//     setCount(count+1)
+//  }
+ 
+//   function decreaseCount(){
+//     setCount(count-1)
+//  }
+//  function resetCount(){
+//     setCount(0)
+//  }
+
+
+//   return <div>
+//     <h1 id = "text">{count}</h1>
+//     <button onClick={increaseCount}> Increase Count</button>
+//     <button onClick={decreaseCount}> Decrease Count</button>
+//     <button onClick={resetCount}> Reset Count</button>
+//   </div>
+// }
+
+// export default App
+
+
+// make a Clock using setinterval
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-      <b>Hi There</b>
       <Counter></Counter>
     </div>
    
@@ -18,23 +60,16 @@ function App() {
 function Counter(){
   const [count, setCount] = useState(0);
 
-  function increaseCount(){
-    setCount(count+1)
- }
- 
-  function decreaseCount(){
-    setCount(count-1)
- }
- function resetCount(){
-    setCount(0)
- }
+
+useEffect(function(){  setInterval(function(){
+        setCount(count =>count+1);
+  },1000)
+  console.log("mounted")
+},[]);
 
 
   return <div>
     <h1 id = "text">{count}</h1>
-    <button onClick={increaseCount}> Increase Count</button>
-    <button onClick={decreaseCount}> Decrease Count</button>
-    <button onClick={resetCount}> Reset Count</button>
   </div>
 }
 
